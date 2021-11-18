faa31b0a19f58ac97d7223212f39f13a396ced00
sunilsuraci <suracisunil@gmail.com>
2021-11-18 16:22:14 +0100

    secondo commit

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -7,17 +7,20 @@ public class StampaNumeriDispari {
 
 	public static void main(String[] args) {
 	
-		
+		//inserire il numero
 		System.out.println("Inserisci il numero: ");
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		
-		//System.out.println(n);
+		//generatore random
 		int[] numero = new int[n];
 		Random random = new Random();
 		for (int i=0; i < numero.length; i++) {
 			numero[i]= random.nextInt(100);
+			
+			if(numero[i] % 2 != 0)
 			System.out.println(numero[i]);
+			
 		}
 		
 		
